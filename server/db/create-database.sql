da390f7807bd1ce19e52fc05750f78680114d049
Jakub Albert Ferenc <jakubferenc1911@gmail.com>
2023-10-20 14:32:34 +0200

    feat: add date

--- server/db/create-database.sql
+++ server/db/create-database.sql
@@ -1,8 +1,24 @@
+DROP TABLE [IF EXISTS] jobs
 CREATE TABLE IF NOT EXISTS jobs
 (
     id SERIAL,
     title text,
     company text,
     description text,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
 );
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON jobs
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
--- server/resolvers.ts
+++ server/resolvers.ts
@@ -5,7 +5,17 @@ export const resolvers = {
   Query: {
     greeting: () => 'Hello GraphQL World!',
     jobs: () => getJobs(),
-    job: (_root: object, args: {id: string}) => getJob(args.id),
+    job: async (_root: object, args: { id: string }) => {
+      const job = await getJob(args.id);
+      return {
+        code: 200,
+        success: true,
+        message: 'Job fetched successfully',
+        job: {
+          ...job,
+        }
+      }
+    },
   },
   Mutation: {
     addJob: async (_: object, args: { newValues: object }) => {
