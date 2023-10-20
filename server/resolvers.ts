66acd08966b9618dd816ceccc5efcd8baa14a79a
Jakub Albert Ferenc <jakubferenc1911@gmail.com>
2023-10-20 16:16:03 +0200

    feat: update servers to refresh, add date prop

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -4,16 +4,23 @@ import { getJobs, getJob, addJob } from './db/index.ts';
 export const resolvers = {
   Query: {
     greeting: () => 'Hello GraphQL World!',
-    jobs: () => getJobs(),
+    jobs: async () => {
+      const jobs = await getJobs();
+      console.log(jobs);
+      return jobs?.map((job) => {
+        return {
+          ...job,
+          date: new Date(job.created_at).toLocaleString('cs-CZ'),
+        }
+      });
+    },
     job: async (_root: object, args: { id: string }) => {
       const job = await getJob(args.id);
       return {
         code: 200,
         success: true,
         message: 'Job fetched successfully',
-        job: {
-          ...job,
-        }
+        job,
       }
     },
   },
