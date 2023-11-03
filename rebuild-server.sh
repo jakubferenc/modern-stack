c753bdaa47394a741367f735c1fbf20f28781ddf
Jakub Albert Ferenc <jakubferenc1911@gmail.com>
2023-11-03 17:08:36 +0100

    chore: force update rebuild

--- rebuild-server.sh
+++ rebuild-server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd server
+docker-compose stop
+docker-compose rm
 rm -rf ~/.docker/config.json
 rm -rf db-data
 docker build . -t modern-stack-server
-docker-compose stop
-docker-compose rm
-docker-compose up -d --force-recreate
+docker-compose up --force-recreate
