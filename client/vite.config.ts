56857d72ea9fbc289831ba249d2752f8e9f2e5ea
Jakub Albert Ferenc <jakub.ferenc@spaceflow.io>
2023-11-15 15:36:20 +0100

    chore: format

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path'
+import { resolve } from 'path';
 
 export default defineConfig(({}) => {
   return {
     plugins: [vue()],
-      resolve: {
-        alias: [
-          { find: "@", replacement: resolve(__dirname, "./src") },
-          { find: "@@", replacement: resolve(__dirname, "./") }
-        ]
-      }
-    };
+    resolve: {
+      alias: [
+        { find: '@', replacement: resolve(__dirname, './src') },
+        { find: '@@', replacement: resolve(__dirname, './') },
+      ],
+    },
+  };
 });
